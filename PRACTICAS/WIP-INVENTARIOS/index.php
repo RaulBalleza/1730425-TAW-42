1198d86bc7764996fbf3fb616438b3ea8b2f08bd
RaulBalleza <1730425@upv.edu.mx>
2020-06-04 08:26:16 -0500

    CLASE JUEVES

--- PRACTICAS/WIP-INVENTARIOS/index.php
+++ PRACTICAS/WIP-INVENTARIOS/index.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
+/*Se activa el almacenamiento del bufer para poder acceder a los valores guardados en los arreglos asociativos $_GET y $_SESSION sin ningun problema*/ 
 
-	ob_start();
-
+	 ob_start();
+	 /*Llamada al archivo que contiene los controladores y modelos que se necesitan para que el sistema funcione correctamente*/
 	require_once("controllers/controller.php");
-	require_once("models/enlaces.php");
-	require_once("models/crud.php");
+ 	require_once("models/enlaces.php");
+ 	require_once("models/crud.php");
 
-	$mvc = new MvcController();
-	$mvc -> plantilla();
-
+ 	$mvc = new MvcController();
+ 	/*Llamada a la plantilla del sistema*/
+ 	$mvc ->plantilla();
 ?>
